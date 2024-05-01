7607f58347f43e9dde9753edeaf4978c7fd95adc
yongin <rladls6412@naver.com>
2024-05-02 00:25:18 +0900

    Feat: 목업폴더생성 및 파일추가

---
+++ src/mocks/ReactionList.js
@@ -0,0 +1,34 @@
+const reactions = {
+  count: 5,
+  next: null,
+  previous: null,
+  results: [
+    {
+      id: 6717,
+      emoji: "🥺",
+      count: 11,
+    },
+    {
+      id: 6712,
+      emoji: "U+1F62D",
+      count: 10,
+    },
+    {
+      id: 6714,
+      emoji: "😆",
+      count: 4,
+    },
+    {
+      id: 6711,
+      emoji: "U+1F600",
+      count: 3,
+    },
+    {
+      id: 6713,
+      emoji: "U+1F389D",
+      count: 1,
+    },
+  ],
+};
+
+export default reactions;
