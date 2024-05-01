aa5971a80dbcfd61922d17fde7fc9ef5926a47df
YoungUnKim <162089313+YoungUnKim@users.noreply.github.com>
2024-05-01 23:39:07 +0900

    Feat : 버튼 컴포넌트 추가

--- src/components/UI/PrimaryButton.jsx
+++ src/components/UI/PrimaryButton.jsx
@@ -1,5 +1,35 @@
-function PrimaryButton() {
-  return <>PrimaryButton Component</>;
+import styled from "styled-components";
+
+const Button = styled.button`
+  width: 208px;
+  height: 56px;
+  background-color: #9935FF;
+  border-radius: 12px;
+  padding: 14px 24px;
+  font-size: 18px;
+  font-weight: 700;
+  color: #ffffff;
+
+  &:hover {
+    background-color: #861DEE;
+  }
+
+  &:focus {
+    background-color: #6E0AD1;
+    border: 2px solid #5603A7;
+  }
+
+  &:disabled {
+    background-color: background: #CCCCCC;
+  }
+
+  &:pressed {
+    background-color: #6E0AD1;
+  }
+`;
+
+function PrimaryButton({ text }) {
+  return <Button type="submit">{text}</Button>;
 }
 
 export default PrimaryButton;
