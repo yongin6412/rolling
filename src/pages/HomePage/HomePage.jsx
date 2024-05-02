186b93b099b1ac80b4c0078767eb332961740087
YoungUnKim <162089313+YoungUnKim@users.noreply.github.com>
2024-05-02 11:32:17 +0900

    Remove : 기능 테스트 부분 삭제

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,5 @@
-import PrimaryButton from "../../components/UI/PrimaryButton";
-
 function HomePage() {
-  return (
-    <div className="page-wrapper">
-      <PrimaryButton type="button">버튼테스트</PrimaryButton>
-    </div>
-  );
+  return <div className="page-wrapper">HomePage</div>;
 }
 
 export default HomePage;
