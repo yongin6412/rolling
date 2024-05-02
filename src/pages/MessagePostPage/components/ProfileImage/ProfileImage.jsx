1a52178cbf4c5b11ea80d947187c0200baa65549
wayandway <wayandway@kakao.com>
2024-05-03 01:48:08 +0900

    Refactor: 이미지 url 값 onChange  핸들러 함수 수정

--- src/pages/MessagePostPage/components/ProfileImage/ProfileImage.jsx
+++ src/pages/MessagePostPage/components/ProfileImage/ProfileImage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { getImages } from "../../../../services/api";
 import styles from "./ProfileImage.module.scss";
 
@@ -12,10 +12,8 @@ function ProfileImage({ name, onChange }) {
     setImgUrls(imgUrlArray.slice(1));
   };
 
-  const handleUrlChange = (e) => {
-    const selectedUrl = e.target.value;
-    setSelected(selectedUrl);
-    onChange(e);
+  const handleUrlChange = () => {
+    onChange(name, selected);
   };
 
   const selectUrl = (e) => {
@@ -29,19 +27,23 @@ function ProfileImage({ name, onChange }) {
   return (
     <div className={styles.container}>
       <div>
-        <input
-          className={styles.urlInput}
-          name={name}
-          value={selected}
+        <img
+          alt="선택한 프로필"
+          src={selected}
+          className={styles.selectedAvatar}
           onChange={handleUrlChange}
         />
-        <img src={selected} className={styles.selectedAvatar} />
       </div>
       <div className={styles.avatarsSection}>
         <p>프로필 이미지를 선택해주세요!</p>
         <div className={styles.avatars}>
           {imgUrls.map((url) => (
-            <img src={url} className={styles.avatar} onClick={selectUrl} />
+            <img
+              alt="프로필 목록"
+              src={url}
+              className={styles.avatar}
+              onClick={selectUrl}
+            />
           ))}
         </div>
       </div>
