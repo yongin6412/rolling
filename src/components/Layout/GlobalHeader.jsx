2ed2746ac446f0300c968a4e690551de73e68a4e
yeonilil <gwagseoyeon@gmail.com>
2024-05-02 22:56:20 +0900

    Feat:글로벌 헤더 컴포넌트 생성

--- src/components/Layout/GlobalHeader.jsx
+++ src/components/Layout/GlobalHeader.jsx
@@ -1,5 +1,29 @@
-function GlobalHeader() {
-  return <>GlobalHeader Component</>;
+import headerLogoImg from "../../../src/assets/icons/header-logo.png";
+import styles from "./GlobalHeader.module.css";
+import { Link, useNavigate } from "react-router-dom";
+
+function GlobalHeader({ isCreateButton }) {
+  const navigate = useNavigate();
+  const goToPost = () => {
+    navigate("/post");
+  };
+  return (
+    <div className={styles.header}>
+      <Link to="/">
+        <img
+          className={styles.headerLogo}
+          alt="header logo"
+          src={headerLogoImg}
+        />
+      </Link>
+      <button
+        className={isCreateButton ? styles.createButton : styles.hidden}
+        onClick={goToPost}
+      >
+        롤링 페이퍼 만들기
+      </button>
+    </div>
+  );
 }
 
 export default GlobalHeader;
