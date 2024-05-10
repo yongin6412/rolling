0fe1e3ff34fce0c362843b2e77ea928a9856909a
YoungUnKim <162089313+YoungUnKim@users.noreply.github.com>
2024-05-11 01:06:32 +0900

    메인 페이지 (#25)
    * Feat : 메인페이지 필요한 이미지 추가
    
    * Feat : 홈페이지 컴포넌트 작업
    
    * Feat : 홈페이지 기본 구조 작성
    
    * Style : 스타일 적용 및 반응형 css 작업
    
    * Refactor : 쓸모없는 모듈 삭제후 메인 페이지에 정렬 방식으로 변경
    
    * fix : 반응형 데스크탑 사이즈 에러 반영
    
    * fix : 반응형 데스크탑 사이즈 에러 반영
    
    * fix : 테블릿 반응형 및 모바일 반응형 타이틀 글자 띄어쓰기 반영
    
    * fix : Link 버튼 공용 버튼 형식으로 변경
    
    * Feat : 메인페이지 필요한 이미지 추가
    
    * Feat : 홈페이지 컴포넌트 작업
    
    * Refactor : 쓸모없는 모듈 삭제후 메인 페이지에 정렬 방식으로 변경
    
    * fix : 버튼 마진 반영

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,94 @@
+import IntroEmoji from "../../assets/images/Intro_Emoji.png";
+import IntroRollingPaper from "../../assets/images/Intro_RollingPaper.png";
+import styles from "./HomePage.module.scss";
+import PrimaryButton from "../../components/UI/PrimaryButton";
+import { useEffect, useRef, useState } from "react";
+
 function HomePage() {
-  return <div className="page-wrapper">HomePage</div>;
+  function handleClick(e) {
+    window.location.href = "/list";
+  }
+
+  const widthMax = useRef(false);
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+  });
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (windowSize.width < 1248) {
+      widthMax.current = true;
+    } else {
+      widthMax.current = false;
+    }
+  }, [windowSize]);
+
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+    });
+  };
+
+  return (
+    <div className={styles.home_wrapper}>
+      <section className={styles.contents}>
+        <div className={`${styles.intro_card} ${styles.rolling}`}>
+          <div className={styles.intro_card_content}>
+            <div className={styles.intro_card_badge}>Point.01</div>
+            <h2 className={styles.intro_card_title}>
+              누구나 손쉽게, 온라인
+              <br className={styles.line_break} /> 롤링 페이퍼를 만들 수 있어요
+            </h2>
+            <p className={styles.intro_card_description}>
+              로그인 없이 자유롭게 만들어요.
+            </p>
+          </div>
+          <div className={styles.intro_image_rolling_container}>
+            <img
+              src={IntroRollingPaper}
+              alt="인트로 롤링페이퍼 이미지"
+              className={styles.intro_image_rolling}
+              fetchpriority="high"
+            />
+          </div>
+        </div>
+        <div className={`${styles.intro_card} ${styles.emoji}`}>
+          <div className={styles.intro_card_content}>
+            <div className={styles.intro_card_badge}>Point.02</div>
+            <h2 className={styles.intro_card_title}>
+              서로에게 이모지로 감정을
+              <br className={styles.line_break} /> 표현해보세요
+            </h2>
+            <p className={styles.intro_card_description}>
+              롤링 페이퍼에 이모지를 추가할 수 있어요.
+            </p>
+          </div>
+          <div className={styles.intro_image_emoji_container}>
+            <img
+              src={IntroEmoji}
+              alt="인트로 이모지 이미지"
+              className={styles.intro_image_emoji}
+              fetchpriority="high"
+            />
+          </div>
+        </div>
+      </section>
+      <PrimaryButton
+        onClick={handleClick}
+        type={"button"}
+        WidthMax={widthMax.current}
+      >
+        구경해보기
+      </PrimaryButton>
+    </div>
+  );
 }
 
 export default HomePage;
